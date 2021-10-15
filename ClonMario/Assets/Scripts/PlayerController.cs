85c23dca661476e0e664c2ca942740e079d908d6
IgnacioC44 <2788936@gmail.com>
2021-10-15 11:48:50 -0400

    Movimiento izquierda derecha

---
+++ ClonMario/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public int speed = 5;
+    private bool FacingRight = true;
+    private float horizontalMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (horizontalMove < 0.0f && FacingRight)
+        {
+            FlipPlayer();
+        }
+        if (horizontalMove > 0.0f && !FacingRight)
+        {
+            FlipPlayer();
+        }
+    }
+    void FlipPlayer()
+    {
+        FacingRight = !FacingRight;
+        Vector2 playerScale = gameObject.transform.localScale;
+        playerScale.x *= -1;
+        transform.localScale = playerScale;
+    }
+}
