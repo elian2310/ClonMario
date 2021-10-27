4dee7ebc7a360f008887f0ce32ed5d480073156e
SantiSuaznabar <Santiagosuaznabar13@gmail.com>
2021-10-26 23:41:52 -0400

    Buzzy Beetle + Caparazon

---
+++ ClonMario/Assets/Scripts/BBShell.cs
@@ -0,0 +1,150 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BBShell : MonoBehaviour
+{
+    private int speed;
+    private bool moveRight;
+
+    private bool shellAttack;
+
+    public Transform bBSpawn;
+    public GameObject bB;
+
+    GameObject player;
+
+    private Rigidbody2D rb;
+
+    private bool cameBack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("player");
+        rb = GetComponent<Rigidbody2D>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveRight)
+        {
+            transform.Translate(Time.deltaTime * speed , 0, 0);
+        }
+        else 
+        {
+            transform.Translate(Time.deltaTime * -speed, 0, 0);
+        }
+
+        if (speed == 0)
+        {
+            StartCoroutine("CameBack");
+        }
+        else
+        {
+            StopCoroutine("CameBack");
+        }
+    }
+
+    private IEnumerator CameBack()
+    {
+        yield return new WaitForSeconds(5f);
+
+        if (cameBack)
+        {
+            yield return new WaitForSeconds(2f);
+            cameBack = false;
+        }
+        else
+        {
+            Instantiate(bB, bBSpawn.position, bBSpawn.rotation);
+            Destroy(gameObject);
+            StopCoroutine("CameBack");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("player"))
+        {
+            if (shellAttack)
+            {
+                float yOffset = 0.5f;
+                if (transform.position.y + yOffset < collision.transform.position.y)
+                {
+                    player.GetComponent<Rigidbody2D>().velocity = Vector2.up * 7;
+                    speed = 0;
+                    shellAttack = false;
+                    gameObject.tag = "shell";
+                }
+                else
+                {
+                    //Descomentar cuando haya Flor y Hongo
+                   /* if (PlayerController.growUp)
+                    {
+                        if (PlayerController.isFlowerUp)
+                        {
+                            PlayerController.isFlowerUp = false;
+                        }
+                        PlayerController.growUp = false;
+                    }
+                    else
+                    {*/
+                        PlayerController.death = true;
+                    //}
+                }
+            }
+            else
+            {
+                float xOffset = 0.5f;
+                if (transform.position.x + xOffset < collision.transform.position.x)
+                {
+                    speed = 4;
+                    moveRight = false;
+                    shellAttack = true;
+                    gameObject.tag = "shellAttack";
+                }
+                else
+                {
+                    speed = 4;
+                    moveRight = true;
+                    shellAttack = true;
+                    gameObject.tag = "shellAttack";
+                }
+            
+            }
+        }
+
+        if (shellAttack)
+        {
+            if (collision.CompareTag("tube"))
+            {
+                if (moveRight)
+                {
+                    moveRight = false;
+                }
+                else
+                {
+                    moveRight = true;
+                }
+            }
+        }
+        if (collision.gameObject.CompareTag("shellAttack"))
+        {
+            Vector2 goombaScale = gameObject.transform.localScale;
+            goombaScale.y = -1;
+            transform.localScale = goombaScale;
+            speed = 0;
+            GetComponent<Collider2D>().isTrigger = true;
+
+            float countdown = 0.5f;
+            countdown -= Time.deltaTime;
+            if (countdown > 0)
+            {
+                rb.velocity = new Vector2(1, 5);
+            }
+        }
+    }
+}
---
+++ ClonMario/Assets/Scripts/BuzzleBeetleController.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuzzleBeetleController : MonoBehaviour
+{
+
+    public int speed;
+    private bool moveRight;
+
+    public Animator animator;
+
+    public Transform shellSpawn;
+    public GameObject shell;
+
+    private GameObject player;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveRight)
+        {
+          
+            transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+        }
+        else
+        {
+            transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+        }
+
+        if (transform.position.y < -30)
+        {
+            Death();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("tube") || collision.gameObject.CompareTag("enemies") || collision.CompareTag("shell"))
+        {
+            if (moveRight)
+                Flip();
+            else
+                Flip();
+        }
+
+        if (collision.CompareTag("player"))
+        {
+            float yOffset = 0.5f;
+            if (transform.position.y + yOffset < collision.transform.position.y)
+            {
+                player.GetComponent<Rigidbody2D>().velocity = Vector2.up * 7;
+                Instantiate(shell, shellSpawn.position, shellSpawn.rotation);
+                Destroy(gameObject);
+            }
+            else
+            {
+                //Descomentar cuando haya flor y hongo
+                /*if (PlayerController.growUp)
+                {
+                    if (PlayerController.isFlowerUp)
+                    {
+                        PlayerController.isFlowerUp = false;
+                    }
+                    PlayerController.growUp = false;
+                }
+                else*/
+                //{
+                    PlayerController.death = true;
+                //}
+            }
+        }
+        
+        if (collision.CompareTag("shellAttack"))
+        {
+            animator.SetBool("Death", true);
+            GetComponent<Collider2D>().isTrigger = true;
+
+            float countdown = 0.5f;
+            countdown -= Time.deltaTime;
+            if (countdown > 0f)
+            {
+                rb.velocity = new Vector2(1f, 5f);
+            }
+        }
+    }
+
+    private void Flip()
+    {
+        moveRight = !moveRight;
+        Vector2 ls = gameObject.transform.localScale;
+        ls.x *= -1;
+        transform.localScale = ls;
+    }
+
+    void Death()
+    {
+        Destroy(gameObject);
+    }
+}
